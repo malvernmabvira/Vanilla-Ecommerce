9a442fda3048f0ea1682f73eb2959baaca60847c
malvernmabvira <malvernmabvira@gmail.com>
2022-05-07 02:02:05 +0300

    changed the product list from static to dynamic by creating data.js file to hold object of produt list, in homescreen.js i created a method to render product list from data.js and created app.js to add window eventlistener using router method that implements render method from homescreen.js

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export default{
+    products: [
+        {
+            _id: '1',
+            name: 'Sports Shirts From Nike',
+            category: 'Shirts',
+            image: '/images/product-1.jpg',
+            price: 55,
+            brand: 'Nike',
+            rating: 4.5,
+            numOfReviews: 10,
+            stockSize: 6
+        },
+        {
+            _id: '2',
+            name: 'Sporting Shoes HRX Black Trainers',
+            category: 'Shoes',
+            image: '/images/product-2.jpg',
+            price: 80,
+            brand: 'HRX',
+            rating: 4.8,
+            numOfReviews: 6,
+            stockSize: 9
+        },
+        {
+            _id: '3',
+            name: 'Sporting Trackbottom Grey For Men',
+            category: 'TrackSuits',
+            image: '/images/product-3.jpg',
+            price: 68,
+            brand: 'Nike',
+            rating: 3.8,
+            numOfReviews: 16,
+            stockSize: 13
+        },
+        {
+            _id: '4',
+            name: 'Sporting TrackSuit',
+            category: 'TrackSuits',
+            image: '/images/product-4.jpg',
+            price: 75,
+            brand: 'Gucci',
+            rating: 4.5,
+            numOfReviews: 7,
+            stockSize: 14
+        },
+        {
+            _id: '5',
+            name: 'High Angle Grey Sneakers',
+            category: 'Shoes',
+            image: '/images/product-5.jpg',
+            price: 95,
+            brand: 'Adidas',
+            rating: 5,
+            numOfReviews: 20,
+            stockSize: 24
+        },
+        {
+            _id: '6',
+            name: 'Red Puma T-Shirt',
+            category: 'Shirts',
+            image: '/images/product-6.jpg',
+            price: 55,
+            brand: 'Puma',
+            rating: 4.7,
+            numOfReviews: 8,
+            stockSize: 6
+        }
+    ],
+};
